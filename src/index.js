f5154fb54b31c1aab83f9aab5b2ce0c3a4c7f7f0
Priyank <152207361+priyankpriyank@users.noreply.github.com>
2025-05-14 23:23:59 -0400

    Updated Index file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // You can use Tailwind or your existing CSS
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
