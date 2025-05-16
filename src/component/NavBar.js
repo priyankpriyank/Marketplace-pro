583e2924a7e8580bcdc84572ffb5c8957e2de406
Priyank <152207361+priyankpriyank@users.noreply.github.com>
2025-05-15 22:59:12 -0400

    Refactor Navbar component and css styles

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -2,13 +2,14 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">🛒 Marketplace</h1>
+    <nav className="bg-blue-600 p-4 text-white flex justify-between">
+      <Link to="/" className="font-bold text-xl">Marketplace</Link>
       <div className="space-x-4">
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/add" className="hover:underline">Add Listing</Link>
-        <Link to="/inbox" className="hover:underline">Inbox</Link>
-        <Link to="/login" className="hover:underline">Logout</Link>
+        <Link to="/">Home</Link>
+        <Link to="/add">Add</Link>
+        <Link to="/inbox">Inbox</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </div>
     </nav>
   );
